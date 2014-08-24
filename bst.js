51a363c7dfb088a7969a9e902f7d3864dbfc9529
Neil Volungis <nvolungis@Neil-MacBookPro.(none)>
2014-08-24 18:22:25 -0400

    added a remove method using the hibbard deletion algorithm

--- bst.js
+++ bst.js
@@ -56,7 +56,7 @@
 
     traverse: function(callback){
       function in_order(node){
-        if(node.left){
+        if( node.left){
           in_order(node.left); 
         } 
 
@@ -67,7 +67,7 @@
         }
       } 
 
-      in_order(this.root);
+      if(!!this.root) in_order(this.root);
     },
 
     to_array: function(){
@@ -100,6 +100,111 @@
       });
 
       return len;
+    },
+
+    remove: function(value){
+      var found   = false,
+          parent  = null,
+          current = this.root,
+          child_count,
+          replace_right = true,
+          replacement = {},
+          replacement_parent = {};
+
+       //find the node
+       while(!found && current){
+         if( value < current.value){
+           parent = current;
+           current = current.left;
+         }else if(value > current.value){
+           parent = current;
+           current = current.right;
+         }else {
+           found = true;
+         }
+       }
+
+       if(!found) return;
+
+        child_count = (!!current.left ? 1:0) + (!!current.right ? 1:0);
+
+        if(current === this.root){
+          switch(child_count){
+          case 0:
+            this.root = null;
+            break;
+
+          case 1:
+            this.root = !!current.left ? current.left : current.right;
+            break;
+  
+          case 2:
+            replace_right = true;
+            replacement = current.right;
+            replacement_parent = current
+
+            while(!!replacement.left){
+              replace_right = false;
+              replacement_parent = replacement;
+              replacement = replacement.left;
+            }
+
+            //remove the smallest value from it's parent
+            if(replace_right){
+              replacement_parent.right = null; 
+            }else{
+              replacement_parent.left = null; 
+            }
+
+            //set the value of the node to replace to the smallest value from it's subtree
+            current.value = replacement.value;
+
+            break;
+          }
+        }else{
+          switch(child_count){
+          case 0:
+            if(current.value < parent.value){ //null out left pointer;
+              parent.left = null; 
+            }else{
+              parent.right = null;  
+            }
+            break;
+
+          case 1:
+            if(current.value < parent.value){
+              parent.left = !!current.left ? current.left : current.right;
+            }else{
+              parent.right = !!current.left ? current.left : current.right;
+            } 
+            break;
+
+          case 2:
+
+            //find the smallest value in the right subtree of the node to remove
+            var replace_right = true;
+            replacement = current.right;
+            replacement_parent = current
+
+            while(!!replacement.left){
+              replace_right = false;
+              replacement_parent = replacement;
+              replacement = replacement.left;
+            }
+
+            //remove the smallest value from it's parent
+            if(replace_right){
+              replacement_parent.right = null; 
+            }else{
+              replacement_parent.left = null; 
+            }
+
+            //set the value of the node to replace to the smallest value from it's subtree
+            current.value = replacement.value;
+
+            break;
+        }
+      }
     }
   };
 
